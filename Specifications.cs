b7772832adc95a4b61992014b926f1d3508e7105
CesarRosemberg <55363596+CesarRosemberg@users.noreply.github.com>
2020-06-29 14:32:53 -0400

    Atributo de Books.cs
    Como a especificação é uma lista, é necessário criar uma classe e referenciá la como um atributo de Books.cs

---
+++ Specifications.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace BooksJson
+{
+    public class Specifications
+    {
+        public string originallyPublished { get; set; }
+        public string author { get; set; }
+        public int pageCount { get; set; }
+        public List<string> illustrator { get; set; }
+        public List<string> genres { get; set; }
+    }
+}
